d102a13dff84cf96b20389956ad5c0b075ae9821
Andres David <andres.pena@linktic.com>
2026-07-31 18:54:10 -0500

    ✨ feat: add layout with top navbar

---
+++ app/layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import TopNavbar from '~/components/layout/TopNavbar.vue'
+</script>
+
+<template>
+  <div>
+    <TopNavbar />
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
