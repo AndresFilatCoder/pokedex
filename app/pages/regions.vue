6ef9b3bd73ac19598804ac3c7f09a78abfccf395
Andres David <andres.pena@linktic.com>
2026-08-01 10:15:53 -0500

    ✨ improvement: improve profile and regions pages with "coming soon" fallback states

---
+++ app/pages/regions.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import FallbackDisplay from '~/components/common/FallbackDisplay.vue'
+</script>
+
+<template>
+  <FallbackDisplay
+    image="/images/jigglypuff.png"
+    title="¡Muy pronto disponible!"
+    subtitle="Estamos trabajando para traerte esta sección. Vuelve más adelante para descubrir todas las novedades."
+  />
+</template>
+
+<style lang="scss" scoped></style>
