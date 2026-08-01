05704e01a75ee4d2c9ef6814aef93a44bcbacd36
Andres David <andres.pena@linktic.com>
2026-08-01 10:22:56 -0500

    🎉 chore(code): remove commented code and unused variables

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,9 +5,6 @@ import PokemonCardsContainer from '~/components/PokemonCardsContainer.vue'
 import type { PokemonList } from '~/services/interfaces/pokemon'
 import { usePokemon } from '~/services/usePokemon'
 
-const navbarStore = useNavbarStore()
-const favoritesStore = useFavoritesStore()
-
 const { getPokemonList } = usePokemon()
 
 const loading = ref(true)
@@ -24,42 +21,6 @@ const setPokemonList = async () => {
     loading.value = false
   }
 }
-
-/* const filterPokemons = (filters: Filters) => {
-  if (!pokemonList.value) return
-
-  if (filters.searchText.length) {
-    pokemonList.value.results = pokemonList.value.results.filter(pokemon =>
-      pokemon.name.toLowerCase().includes(filters.searchText.toLowerCase())
-    )
-  }
-
-  if (filters.types.length) {
-    pokemonList.value.results = pokemonList.value.results.filter(pokemon =>
-      filters.types.every(type => pokemon.details?.types.some(t => t.type.name === type))
-    )
-  }
-
-  navbarStore.resultsCount = pokemonList.value.results.length
-} */
-
-/* const setIsFavorite = () => {
-  pokemonList.value?.results.forEach(pokemon => {
-    pokemon.isFavorite = favoritesStore.favoritePokemons.some(
-      favorite => favorite.name === pokemon.name
-    )
-  })
-} */
-
-/* watch(
-  () => navbarStore.filters,
-  async () => {
-    await setPokemonList()
-    filterPokemons(navbarStore.filters)
-    setIsFavorite()
-  },
-  { deep: true, immediate: true }
-) */
 
 onBeforeMount(() => setPokemonList())
 </script>
